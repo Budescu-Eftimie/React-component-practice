b22453a09193354de5b08b8f4732b2676a728efc
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-19 04:17:53 +0200

    style: add css rules for emoji.svg

--- src/Instructions.js
+++ src/Instructions.js
@@ -4,10 +4,10 @@ import emoji from "./emoji.svg";
 export default class Instructions extends Component {
     render() {
         return (
-            <>
+            <div className="instructions">
                 <img alt="laughing crying emoji" src={emoji} />
                 <p>Click on an emoji to view emoji short name</p>
-            </>
+            </div>
         );
     }
 }
