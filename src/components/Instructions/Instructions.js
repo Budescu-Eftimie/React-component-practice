c6b6da414bb2302c18862c73ebdf48587b8b536b
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-19 05:03:27 +0200

    refactor:Refactor class component Instructions into functional comonent

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,14 +1,11 @@
-import { Component } from "react";
 import emoji from "./emoji.svg";
 import "./Instructions.css";
 
-export default class Instructions extends Component {
-    render() {
-        return (
-            <div className="instructions">
-                <img alt="laughing crying emoji" src={emoji} />
-                <p>Click on an emoji to view emoji short name</p>
-            </div>
-        );
-    }
+export default function Instructions() {
+    return (
+        <div className="instructions">
+            <img alt="laughing crying emoji" src={emoji} />
+            <p>Click on an emoji to view emoji short name</p>
+        </div>
+    );
 }
